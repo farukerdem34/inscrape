161533091ca928c5a603381915c6d09cba4e008f
Faruk Erdem <farukomererdem2003@gmail.com>
2023-08-09 10:31:54 -0400

    UPDATE argument helps.

--- scraper/userinput.py
+++ scraper/userinput.py
@@ -5,9 +5,9 @@ def getUserInput():
 
         # Username or E-Mail (Sign In)
         group = parser.add_mutually_exclusive_group(required=True)
-        group.add_argument("-u","--username",dest="username",help="Instagram account username.",type=str)
-        group.add_argument("--email",dest="email",help="Account email.",type=str)
-        parser.add_argument("-p","--password",dest="password",help="Instagram account password.",type=str,required=True)
+        group.add_argument("-u","--username",dest="username",help="Username of the Instagram account to login.",type=str)
+        group.add_argument("--email",dest="email",help="Email address name of Instagram account to login.",type=str)
+        parser.add_argument("-p","--password",dest="password",help="The password of the Instagram account to be logged in.",type=str,required=True)
 
         # Verbosisty
         parser.add_argument("-v","--verbose",dest="verbose",action="store_true",help="Verbosity")
@@ -21,7 +21,7 @@ def getUserInput():
         follow_parser = subparsers.add_parser("follow")
         follow_parser.add_argument("--followers",action="store_true")
         follow_parser.add_argument("--followings",action="store_true")
-        follow_parser.add_argument("-t","--target",dest="target",help="Target account name.",type=str,required=False)
+        follow_parser.add_argument("-t","--target",dest="target",help="Username of the account to get the follower and unfollowed list.",type=str,required=True)
         output_group = follow_parser.add_mutually_exclusive_group(required=False)
         output_group.add_argument("-o",dest="output",action="store_true")
         output_group.add_argument("--output",dest="output",type=str)
